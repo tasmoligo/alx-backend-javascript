8ef7e3eb3ba57aacad050a9e44acd3220ce14729
Tasie Ibiam <tasieibiam28@gmail.com>
2024-10-08 03:32:53 +0100

    making variables unable to be overwritten

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 // Makes sure variables aren’t overwritten inside the conditional block.
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (true Or false) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
