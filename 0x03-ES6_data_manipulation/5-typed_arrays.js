0f4f3a28e34ebdd189e54b2a5f284bb7389f2972
Tasie Ibiam <tasieibiam28@gmail.com>
2024-10-12 04:41:31 +0100

    Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+    if (position < 0 || position >= length) {
+        console.log('Position outside range');
+    }
+    const buffer = new ArrayBuffer(length);
+    const int8 = new Int8Array(buffer);
+    int8.set([value], position);
+    return new DataView(buffer);
+}
