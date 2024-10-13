0910d9271ec2105ca7e5ceda40a2e577a82df535
Tasie Ibiam <tasieibiam28@gmail.com>
2024-10-13 04:29:16 +0100

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,8 @@
 export default function returnHowManyArguments(...suppliedArgs) {
-    let supplied = 0;
-    let arg;
-    for (arg of suppliedArgs) {
-        supplied += 1;
-    }
-    return supplied;
+  let supplied = 0;
+  let arg;
+  for (arg of suppliedArgs) {
+      supplied += 1;
+  }
+  return supplied;
 }
