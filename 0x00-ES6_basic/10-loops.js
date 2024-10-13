5cc7bc6a07a62e7b74f72d2378e77e45f7ef750c
Tasie Ibiam <tasieibiam28@gmail.com>
2024-10-13 05:08:38 +0100

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = [];
-  for (let value of array) {
+  const newArray = [];
+  for (const value of array) {
     newArray.push(appendString + value);
   }
   return newArray;
