a49fa989b481067d2aac2490e46dee40055ac8a7
Tasie Ibiam <tasieibiam28@gmail.com>
2024-10-13 04:49:12 +0100

    Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,9 @@
-// uses shorthand method
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-      income,
-      gdp,
-      capita,
-    };
-  
-    return budget;
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
 }
