efe79479b9ed6541127f4924adf9010832996b78
tasmoligo <tasieibiam28gmail.com>
2024-10-20 12:17:12 +0100

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,39 +1,37 @@
-import { tsConstructorType } from "@babel/types"
-
-export default class HolbertonCourse() {
-  constructorType(name, length, students) {
-    if (typeof name === string) {
-      this._name = name;
-    } else {
-      console.log("name must be a string.");
-    }
-    if (typeof number === number) {
-      this._number = number;
-    } else {
-      console.log("number must be a number");
-    }
-    if (Array.isArray(students)) {
-      this._students = students;
-    } else {
-      console.log("students is not an array os strings.");
-    }
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a string');
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array of string');
+    this._name = name;  
+    this._number = number;
+    this._students = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
     this._name = value;
   }
+
   get length(){
     return this._length;
   }
+
   set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a string');
     this._length = value;
   }
-  get students(){
+
+  get students() {
     return this._students;
   }
+
   set students(value) {
+    if (!Array.isArray(value)) throw new TypeError('Students must be an array of string');
     this._students = value;
   }
 }
